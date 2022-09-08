446159d46abd25c59930c9f14340598c7efa5b84
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-09-08 12:44:18 -0400

    Node intro file added, first helloworld using Node

---
+++ intro/helloworld.js
@@ -0,0 +1,23 @@
+/*
+Welcome to Jonathan's first lines of node programming. 
+
+This directory will be dedicated to Node development, this is 
+a continuation of basic Javascript Learning. 
+
+*/
+
+
+const http = require('http'); 
+const port = process.env.port || 3000
+
+const server = http.createServer((req,res) =>{
+  res.writeHead(200, {'Content-Type': 'text/plain'})
+  res.end('This is my first example of node programming ')
+})
+
+
+server.listen(port, () => console.log(`server started on port ${port};` + 
+' press Ctrl-C to terminate'))
+
+
+
