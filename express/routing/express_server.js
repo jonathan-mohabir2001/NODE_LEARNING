8392e475f75fb7e518b2eaf1dbe5685d0241dc59
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-09-24 20:56:37 -0400

    express routing covered, basics introduced and understood

--- express/routing/express_server.js
+++ express/routing/express_server.js
@@ -5,21 +5,23 @@ Express is very helpful for creating servers using javscript knowledge.
 
 This js file will demonstrate routing functionality upon requests. 
 */
-const express = require('express'); 
-// express constant is assigend the module express from node. 
-const PORT = process.env.PORT || 8000; 
+const express = require('express');
+// express constant is assigend the module express from node.
+const PORT = process.env.PORT || 8000;
 // a port is declared and is set to 8000. Local host will be accessed to test these outputs
-const app = express(); 
-// app will be accessing methods of express 
+const app = express();
+// app will be accessing methods of express
 app.listen(PORT, () => {
-  console.log(`Server started on ${PORT}`); 
-})
-// this statement will have a log out to the console to show what port the app is set to. 
+  console.log(`Server started on ${PORT}`);
+});
+// this statement will have a log out to the console to show what port the app is set to.
 
-const router = require('./routing'); 
-// this router constant is assigned the routing file.  All the functionality from that routing file is now accessible. 
+const router = require('./routing');
+// this router constant is assigned the routing file.  All the functionality from that routing file is now accessible.
 
-app.use('/' , router); 
+app.use('/', router);
 /*
-
+The statment above: 
+Is utilizing the USE method of express, and is assigned two parameters ( everything from the home page, and then is assigned 
+  routing functionaity through the ROUTER constant. )
 */
--- express/routing/routing.js
+++ express/routing/routing.js
@@ -3,11 +3,21 @@ Routing functionality is written inside this Js file.
 */
 
 const express = require('express');
-
+// express constant assigned express module.
 const routes = express.Router();
+// routes constants assigned Router() from accessed using dot notation
 
+const greeting =
+  'Hi there my name is Jonathan, I just wrote some express code!';
+// greeting constant declared for message
 
+routes.get('/', (req, res) => {
+  /*.get is passed two parameters.
+   the Home path, and the request and response is then passed
+  to an arrow function that returns an end message.
+  */
+  res.end(`<h1>Server says:${greeting}`);
+});
 
-
-module.exports = routes; 
+module.exports = routes;
 // this statement allows for the routing functionality to be exported into the server.js file.
