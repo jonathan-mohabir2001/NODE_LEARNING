bff3715b607628ec472a431d3f604119d561abc0
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-09-30 14:40:47 -0400

    code updated, request and response methods and properties reviewed.

--- express_review/req_res_review/index.js
+++ express_review/req_res_review/index.js
@@ -1,28 +1,63 @@
+/*
+This node file will cover req, and res methods and properties. 
+*/
 const express = require('express');
-// express module imported
+// express module imported.
 const app = express();
-// app assigned express methods
+// app assigned express methods.
 const PORT = process.env.PORT || 8000;
-// port set to listen into 8000
+// port constant set.
+
 app.get('/about', (req, res) => {
-  res.send('about');
-}); // sends to about page
+  res.send('This is the about page.');
+});
+// this response sends info about the about page.
 
 app.get('/error', (req, res) => {
   res.status(500);
-  res.send('You have an error');
-}); // if the status code is read as 500, the response will send an error message.
+  // status sets the status code of the HTTP status code.
+  res.send('You have an error!');
+});
+/*
+The response from the network tab in the developer tools has returned 
+a status code of 500; internal server error. 
+*/
+app.get('/text', (req, res) => {
+  res.type('text/plain');
+  // the response type is set to a plain fashioned text
+  res.send('This is some text');
+});
+// the response above sends a plain message with it's type set to plain text.
 
-app.get('/greeting', (req, res) => {
-  res.send('Greeting to user', {
-    message: 'HELLO',
+//app.get('/greeting', (req, res) => {
+//res.status(200);
+//res.send('Greeting is', {
+//  MessageChannel: 'Hello there',
+//});
+//});
+// code above had an invalid status code.
+
+app.get('/', (req, res) => {
+  res.send('You have landed on home');
+});
+// get response returning message for user landing onto the home.
+
+//error handler below.
+app.use((err, req, res, next) => {
+  console.error('** SERVER ERROR,:' + err.message);
+  res.status(500).render('08-error', {
+    message: 'You should not have clicked that.',
   });
 });
 
-// home page routed, and listen method coded.
-app.get('/', (req, res) => {
-  res.send('<h1>You have landed on the base home page</h1>');
+// 404 handler below
+
+app.use((req, res) => {
+  res.status(404).send('you got a 404. ');
 });
+// any thing that isn't set as a route will be redirected to this send response.
+
 app.listen(PORT, () => {
-  console.log(`The server has started on ${PORT}`);
+  console.log(`Server has started on ${PORT}, press ctrl+c to end`);
 });
+// listen method set to listen into port.
