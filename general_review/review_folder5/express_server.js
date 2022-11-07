bf0dd09068e2413fdf03ca5ae1355782e5855e58
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-11-07 08:30:38 -0500

    new commit, repo updated, revisiting node textbook

---
+++ general_review/review_folder5/express_server.js
@@ -0,0 +1,9 @@
+const express = require('express'); 
+const app = express();
+const PORT = process.env.PORT || 8000; // default port 8080
+
+
+app.listen(PORT, () => {
+
+  r
+})
--- orielly_book/sample_project/meadowlark/meadowlark.js
+++ orielly_book/sample_project/meadowlark/meadowlark.js
@@ -1,43 +1,37 @@
 /*
-This node file will be the entry point for this project. 
+Meadowlark .js travel site, this js file contains the 
+main code for this server. 
+
+Refer to companion repo if any trouble.
 */
-const express = require('express');
-// The concept of views understood, the handlebars module will be imported below
-// handlebars imported. 
-const app = express();
-const PORT = process.env.PORT || 3000;
-// express method set, app assigned express methods, server set to port 3000
-//error handling is provivded at the bottom. But Routing must be coming before
-app.get('/', (req,res) => {
-  res.type('text/plain')
-  res.send('You have landed on the home page. ')
+const express = require('express'); 
+const app = express(); 
+const PORT = process.env.PORT || 8000; 
+
+
+app.get('/', (req, res) => {
+  res.send('<h1>hey user, welcome to the home </h1>')
+}) 
+app.get('/about', (req, res) => {
+  res.send('<h2>Hey user, welcome to the about page </h2>')
 })
-// this route starts at the beginning directory and renders the landed on about page message 
-app.get('/about' , (req,res) => {
-  res.type('text/plain')
-  res.send('You have landed on the about page.')
+// default routes created. 
+
+app.use('/',(req, res) => {
+  res.status(404)
+  res.send('<h2>Page not found!</h2>')
 })
+app.use('/', (req,res) => {
+  res.status(500)
+  res.send('<h2>Server error!</h2>')
+})
+// custom middleware added
 
-
-
-
-
-// error handling will be created down below. for the entrie app will be created below
-app.use((req, res) => {
-  res.status(500);
-  res.send('<h2>Server error</h2>');
-});
-// server given status code of 500, will return server error message.
-app.use((req, res) => {
-  res.status(404);
-  res.send('<h2>Page not found</h2>');
-});
-// server will render a not found page.
 
 app.listen(PORT, () => {
-  console.log(`The server has started on localhost at port...${PORT}, press 
-  ctrl+c to end.`);
-});
-// listen method set, console will be given information about the localhost server. 
+  console.log(`Server started, running on PORT-${PORT}. Press ctrl+c to end.`)
+})
+
+
 
 
