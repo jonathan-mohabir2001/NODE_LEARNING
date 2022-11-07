5ae986941148e83fcd122478ffbb67c701b54224
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-11-07 10:35:46 -0500

    new commit

--- orielly_book/sample_project/meadowlark/meadowlark.js
+++ orielly_book/sample_project/meadowlark/meadowlark.js
@@ -4,41 +4,51 @@ main code for this server.
 
 Refer to companion repo if any trouble.
 */
-const express = require('express'); 
-const app = express(); 
-const PORT = process.env.PORT || 8000; 
-
-const expressHandlebars = require('express-handlebars'); 
-// hanlde bars module importred. 
-
-
-
-app.engine('.handlebars', expressHandlebars.engine({extname: '.handlebars', defaultLayout:'main'}))
-app.set('view engine', 'handlebars')
-// configuration for handle bars. 
-
-
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
+const expressHandlebars = require('express-handlebars');
+// hanlde bars module importred.
+app.engine(
+  '.handlebars',
+  expressHandlebars.engine({ defaultLayout: 'main.handlebars' })
+);
+app.set('view engine', 'handlebars');
+// configuration for handle bars.
 app.get('/', (req, res) => {
-  res.render('home')
-})
-app.get('/about', (req,res) => {
-  res.render('about')
-})
-// routes updated. These routes are now using the render method of the response. 
-// render is able to read the handle bars files because of the engine and set methods. 
+  res.render('home');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+// routes updated. These routes are now using the render method of the response.
+// render is able to read the handle bars files because of the engine and set methods.
 
+// an array of strings will be created below.
+
+const forutnes = [
+  'Look up, not down',
+  ' Do not Fear what you do not know',
+  'You will have a pleasent surprise',
+];
+// array of messages created. 
+
+
+
+
+app.use(express.static(__dirname + '/public'));
+// setting up the middleware to allow for the serving of static files.
 app.use((err, req, res, next) => {
-  console.log(err.message)
-  res.status(500)
-  res.render('500')
-})
+  console.log(err.message);
+  res.status(500);
+  res.render('500');
+});
+// error handling updated, sending the 505 handlebars page.
 app.use((req, res) => {
-  res.status(404)
-  res.render('404')
-})
-
-
-
+  res.status(404);
+  res.render('404');
+});
+// error handling updated, sending the 404 page.
 app.listen(PORT, () => {
-  console.log(`Server started, running on PORT-${PORT}. Press ctrl+c to end.`)
-})
+  console.log(`Server started, running on PORT-${PORT}. Press ctrl+c to end.`);
+});
