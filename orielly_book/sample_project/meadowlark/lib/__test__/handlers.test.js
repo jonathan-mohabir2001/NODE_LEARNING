5707404dabe211f7200b8be354b481c45710346d
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-11-10 09:44:29 -0500

    test code updated, first test written properly

--- orielly_book/sample_project/meadowlark/lib/__test__/handlers.test.js
+++ orielly_book/sample_project/meadowlark/lib/__test__/handlers.test.js
@@ -1,13 +1,11 @@
 const handlers = require('../handlers'); 
-// the code to be tested must be imported. 
-// in our case we are testing an aspect of the handlers file. 
-test('home page rendering...', () => {
-// test method called with description, each test has one. 
-// then a listener is written and then an arrow function. 
-  const req = {}; 
-  const res = {render:jest.fn()}
-  // request and response objects are needed. Request is empty
-  
+// handlers imported to be tested. 
+
+
+test('home page rendering', () => {
+  const req = {}
+  const res = {render: jest.fn()}
   handlers.home(req, res)
-  expect(res.render.mock.calls[0][0].toBe('home'))
+
+  expect(res.render.mock.calls[0][0]).toBe('home')
 })
