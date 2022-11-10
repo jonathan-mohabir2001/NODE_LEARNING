330647ab4e30a680822a1a5fbcbe7b499bec25d1
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-11-10 11:35:17 -0500

    new commit, tests written.

--- orielly_book/sample_project/meadowlark/lib/__test__/handlers.test.js
+++ orielly_book/sample_project/meadowlark/lib/__test__/handlers.test.js
@@ -12,12 +12,32 @@ test('home page rendering', () => {
   // this is the testing statement. 
 })
 
+test('about page with a fortune to be returned...?', () => {
+  const req = {}
+  // empty request body created. 
+  const res = {render: jest.fn()}
+  // response sends a jest.function 
+  handlers.about(req, res)
+  // the handlers is called but then returns 
+  expect(res.render.mock.calls.length).toBe(1)
+  expect(res.render.mock.calls[0][0]).toBe('about')
+})
 
-test('Is fortune rendered with about page' , () => {
+test('404 handler renders?', () => {
   const req = {}
   const res = {render: jest.fn()}
-  handlers.about(req, res) 
+  handlers.notFound( req, res)
+  expect (res.render.mock.calls.length).toBe(1)
+  expect (res.render.mock.calls[0][0]).toBe('404')
+})
+
+test('505 error handling renders?', () => {
+  const req = {}
+  const res = {render: jest.fn()}
+  const err = new Error('got an error!')
+  handlers.serverError(err, req, res)
 
   expect(res.render.mock.calls.length).toBe(1)
-  expect(res.render.mock.calls).toBe('about')
+  expect(res.render.mock.calls[0][0]).toBe('500')
+
 })
