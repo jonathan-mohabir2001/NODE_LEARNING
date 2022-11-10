0f8c9997938ff9a91e582c9878c93faf747ed9df
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-11-10 10:01:51 -0500

    more testing code to be written...

--- orielly_book/sample_project/meadowlark/lib/__test__/handlers.test.js
+++ orielly_book/sample_project/meadowlark/lib/__test__/handlers.test.js
@@ -5,7 +5,19 @@ const handlers = require('../handlers');
 test('home page rendering', () => {
   const req = {}
   const res = {render: jest.fn()}
+// response is going to conduct a jest function 
   handlers.home(req, res)
-
+  // route to send the correct, page. 
   expect(res.render.mock.calls[0][0]).toBe('home')
+  // this is the testing statement. 
+})
+
+
+test('Is fortune rendered with about page' , () => {
+  const req = {}
+  const res = {render: jest.fn()}
+  handlers.about(req, res) 
+
+  expect(res.render.mock.calls.length).toBe(1)
+  expect(res.render.mock.calls).toBe('about')
 })
