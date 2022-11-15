5b5d467f563207d76dacbeae679fb987ba7534da
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-11-15 08:07:56 -0500

    new commit, mongoose module created

--- general_review/review_folder6/express_server.js
+++ general_review/review_folder6/express_server.js
@@ -1,27 +1,32 @@
-const express = require('express')
-const app = express(); 
-const PORT = process.env.PORT || 8000; 
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 8000;
 
-const handlers = require('./handlers/handlers')
+const handlers = require('./handlers/handlers');
 
-// views set to allow for pug files to be rendered. 
-app.set('view engine', 'pug')
-app.set('views', './views')
+const mongoose = require('mongoose');
+// mongoose imported.
 
+let db = mongoose.connection;
 
-app.get('/', handlers.renderHome)
-// home route 
-app.get('/users', handlers.renderForm)
-// users route to form 
-app.get('/car', handlers.viewCar)
-// view car object route 
-app.get('/developer', handlers.viewDeveloper)
+// views set to allow for pug files to be rendered.
+app.set('view engine', 'pug');
+app.set('views', './views');
 
+app.get('/', handlers.renderHome);
+// home route
+app.get('/users', handlers.renderForm);
+// users route to form
+app.get('/car', handlers.viewCar);
+// view car object route
+app.get('/developer', handlers.viewDeveloper);
 
 app.use(handlers.notFound);
 app.use(handlers.serverError);
-// error handling 
+// error handling
 
 app.listen(PORT, () => {
-  console.log( `Server has started, running on port ${PORT} press ctrl+c to end`)
-})
+  console.log(
+    `Server has started, running on port ${PORT} press ctrl+c to end`
+  );
+});
