e5e357ebb9300f49da88f0f1aebbeb431b0c50fa
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-11-16 10:23:00 -0500

    new commit, mongodb connection established, views directory and files created

--- general_review/school_folder/assignment_02/express_server.js
+++ general_review/school_folder/assignment_02/express_server.js
@@ -1,16 +1,37 @@
+/*
+Assignment 02 created by Jonathan M. 
+
+MongoDb + Express app. Creating an application to create recipe's
+*/
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8000; 
-// 
-
+// Express imported, app set, port set. 
 const path = require('path');
-const fs = require('fs')
+
 // path and filesystem modules imported 
-
 const mongoose = require('mongoose'); 
+mongoose.connect("mongodb://127.0.0.1/FoodRecipe")
+// connection to database established. 
 const db = mongoose.connection; 
+// db set to connection methods. 
+db.once("open", () => {
+  console.log(`Connected to MongoDB Database`)
+})
+db.on("error" , (err) =>{
+  console.log(`We have an error! `)
+})
+
+app.set('view-engine','pug')
 
 
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
+})
 
 
 
