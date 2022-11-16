659f07b0b4ee726e46e227b2573050f440f8ce70
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-11-16 18:17:28 -0500

    database connection established

--- general_review/school_folder/assignment_02/express_server.js
+++ general_review/school_folder/assignment_02/express_server.js
@@ -8,14 +8,19 @@ const app = express()
 const PORT = process.env.PORT || 8000
 const path  = require('path')
 
-/*
-Database setup below using the mongoose module and its methods
-*/
-
-
-
-
-
+// MongoDb config starts here. 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://127.0.0.1/foodstore')
+const db = mongoose.connection 
+db.once("open", () => {
+// runs once and then , logs a connection  message
+  console.log(`Successful connection to MongoDB`)
+})
+db.on("error", (error) => {
+  // "on an " error, log the error message. 
+  console.log(`We have an error! `)
+})
+// MongoDb connection established. 
 
 
 
@@ -24,6 +29,8 @@ Database setup below using the mongoose module and its methods
 app.get('/', (req, res) => {
   res.render('home.pug')
 })
+
+
 app.set('/', path.join(__dirname, 'views'))
 app.set('view-engine', 'pug')
 // view engine set to view pug files. 
