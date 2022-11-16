7c4fe8c1d90b6798b646d3b9fced72134f1e96d2
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-11-16 09:38:14 -0500

    new commit, project for assignemnt 02

--- general_review/school_folder/assignment_02/express_server.js
+++ general_review/school_folder/assignment_02/express_server.js
@@ -1,27 +1,16 @@
-/*
-Assignment 03 created by Jonathan M. 
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 8000; 
+// 
 
-Express app, connecting to MongoDB, using Mongoose for 
-CRUD operations. 
+const path = require('path');
+const fs = require('fs')
+// path and filesystem modules imported 
 
-Routes completed within "handlers" directory under handlers.js
-*/
-
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8000;
-
-const handlers = require('./handlers/handlers')
-// handlers.js file imported to handle get requests 
-
-app.get('/', handlers.homePage)
-
-app.set('view-engine', 'pug');
-// set method to allow rendering of pug files.
-
-app.get('/', )
+const mongoose = require('mongoose'); 
+const db = mongoose.connection; 
 
 
-app.listen(PORT, () => {
-  console.log(`Server has started on port-${PORT}`);
-});
+
+
+
