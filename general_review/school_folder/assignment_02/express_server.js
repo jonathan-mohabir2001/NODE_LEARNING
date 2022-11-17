0d72caf3a972e1b5ad4f3fe7acf21284821aa3d5
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-11-17 12:33:06 -0500

    commit, new pug files created and added

--- general_review/school_folder/assignment_02/express_server.js
+++ general_review/school_folder/assignment_02/express_server.js
@@ -10,18 +10,34 @@ const path = require('path')
 const mongoose = require('mongoose')
 // mongoose imported. 
 
-app.get('/', (req,res) => {
-  res.send('hello world. ')
+mongoose.connect('mongodb://127.0.0.1/foodstore')
+// connection string entered. 
+let db = mongoose.connection; 
+// db assigned mongoose connection methods. 
+
+db.once("open", () => {
+  console.log(`Connection to DB established!`)
 })
+db.on("error" , (error) => {
+  console.log(`We have an error!`)
+})
+//database connection set, error handling created. 
 
 
-
-
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'))
+// middle ware added. Allows urlencoding, json, and serving files from directory public. 
 
 
 app.set('/', path.join(__dirname, 'views')); 
 app.set('view-engine', 'pug')
 // view engine set to view pug files 
+
+app.get('/', (req,res) => {
+  res.render('index.pug')
+})
+
 app.listen(PORT, () => {
   console.log( `Server started on port ${PORT}, ctrl+c to end.`)  
 })
