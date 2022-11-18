2ce5991374c35f457b89f3e12391672b9c768e9a
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-11-18 09:01:09 -0500

    old code removed, new code added

--- general_review/school_folder/assignment_02/public/js/main.js
+++
@@ -1,3 +0,0 @@
-// this js file waits for the document to be ready 
-// J-Query DOM manipulation done within this JS file. 
-
--- general_review/school_folder/assignment_02/routes/recipes.js
+++
@@ -1,9 +0,0 @@
-/*
-This js file contains the routes for the application 
-*/
-
-const express = require('express')
-const router = express.Router(); 
-const {check, validationResult} = require('express-validator')
-// express imported, router set, validator imported. 
-
