7ce04d0d9326551cd768585a38770ce52b1f6d40
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-11-18 11:06:27 -0500

    new commit, required routes are all done and created

--- general_review/school_folder/assignment_02/express_server.js
+++ general_review/school_folder/assignment_02/express_server.js
@@ -49,6 +49,66 @@ app.route("/recipe/delete/:id").get((req,res ) => {
   })
 })
 
+app.route("/recipe/edit/:id").get((req, res)=> {
+  var id = req.params.id;
+  Recipe.findById(id, (error, recipe) => {
+    if(error){
+      res.end("this recipe cannot be edited")
+    }
+    else{
+      res.render('Edit', {name:"edit a recipe", ingredients:["Tomatoe", "Pizza dough", "Pepperoni", "HotPepper", "Onion"]})
+    }
+  })
+
+
+}).post((req, res) => {
+  var id = req.params.id; 
+  var recipe = new Recipe(); 
+  recipe.name = req.body.name; 
+  recipe.description = req.body.description; 
+  recipe.difficulty = req.body.difficulty; 
+  recipe.ingredients = req.body.ingredients; 
+  recipe.steps = req.body.steps
+  console.log(req.body.name)
+  Recipe.updateOne({_id:id}, recipe ,(error) => {
+    res.redirect('/')
+  })
+})
+app.route("/recipe/add").get((req,res) => {
+  res.render('add', {name:"add a recipe",ingredients:["Tomatoe", "Pizza dough", "Pepperoni", "HotPepper", "Onion"]})
+})
+.post((req, res) => {
+  var recipe = new Recipe()
+  recipe.name = req.body.name; 
+  recipe.description = req.body.description; 
+  recipe.difficulty = req.body.difficulty; 
+  recipe.ingredients = req.body.ingredients; 
+  recipe.steps = req.body.steps
+  recipe.save((error) => {
+    if(error){
+      console.log(error)
+      res.end("erorr occured")
+    }
+    else{
+      res.redirect("/")
+    }
+  })
+});
+
+
+app.use(function ( req, res, next) {
+  next(createError(404)); 
+})
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
 
 app.listen(PORT, () => {
   console.log( `Server started on port ${PORT}, ctrl+c to end`)
