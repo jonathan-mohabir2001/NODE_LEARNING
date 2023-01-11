6f62e7b79d50edd583bc74c494ab01727464d07b
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-01-10 21:55:19 -0500

    refreshing review

---
+++ general_review/review_folder12/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express(); 
+const PORT = process.env.PORT ||8000; 
+
+
+
+
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`)
+})
