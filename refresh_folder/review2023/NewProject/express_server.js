0c0300998f74794685a6c6377e8ec06a49a9b7b8
Jonathan Mohabir <jonamohabir416@gmail.com>
2023-01-24 18:41:18 -0500

    bootstrap implemented

--- refresh_folder/review2023/NewProject/express_server.js
+++ refresh_folder/review2023/NewProject/express_server.js
@@ -1,20 +1,33 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 8000; 
+const PORT = process.env.PORT || 8000;
 
+const fs = require('fs');
+const path = require('path');
 
-const fs = require('fs')
-const path = require('path')
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
+//set middle ware to allow pug files to use bootstrap
+app.use(
+  '/css',
+  express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css'))
+);
 
+app.get('/', (req, res) => {
+  res.render('info');
+});
 
-app.set('view engine', 'pug')
-app.use(express.static('public'))
+app.get('/signIn', (req, res) => {
+  res.render('signIn');
+});
 
+app.get('/signUp', (req, res) => {
+  res.render('signUp');
+});
 
-app.get('/', (req, res) => { 
-  res.render('info')
-})
 app.listen(PORT, () => {
-  console.log(`server started on PORT ${PORT}`)
-})
+  console.log(`server started on PORT ${PORT}`);
+});
